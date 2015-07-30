84f3b8f47d066725f4a279059890f29a7ab52e7f
tindaio <ccfiel@csharpinc.com>
2015-07-30 08:38:07 +0800

    Update railtie.rb

--- lib/font_assets/railtie.rb
+++ lib/font_assets/railtie.rb
@@ -8,11 +8,12 @@ module FontAssets
       config.font_assets.origin ||= "*"
       config.font_assets.options ||= { allow_ssl: true }
 
-      insert_target = if defined?(ActionDispatch::Static)
-        'ActionDispatch::Static'
-      else
-        'Rack::Runtime'
-      end
+      # insert_target = if defined?(ActionDispatch::Static)
+      #   'ActionDispatch::Static'
+      # else
+      #   'Rack::Runtime'
+      # end
+       insert_target = 'Rack::Runtime'
 
       app.middleware.insert_before insert_target, FontAssets::Middleware, config.font_assets.origin, config.font_assets.options
     end
